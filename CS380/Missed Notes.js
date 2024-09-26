05951f8d5ce4e587d77c1afd54ea07614d7a8c61
Justin <jkubicz@nmu.edu>
2024-09-26 10:45:32 -0400

    added missed notes from 9/25

---
+++ CS380/Missed Notes.js
@@ -0,0 +1,17 @@
+console.log("Hello, world!\n");
+
+console.log([1,2,3].map(function(x){return x*x}))
+
+
+redGuy = {name:"Santa"};
+
+redGuy.speak = function(){console.log("\nSanta says: ho ho ho")};
+
+redGuy.speak();
+
+bob = {
+    name:"Santa",
+    speak: function(){console.log("\Bob says: Wassup")}
+}
+bob.speak();
+
